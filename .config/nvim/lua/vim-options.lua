c5b26d0a3c56ba50e22acc4852af43b3fb0770d4
Steve Xuereb <hi@steve.mt>
2026-06-14 06:12:31 +0200

    feat(nvim): disable wrapping

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -6,6 +6,7 @@ vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
 vim.o.smartindent = true
+vim.o.wrap = false
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = "menuone,noselect"
